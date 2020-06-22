f5a13f88aed4c675de4a67a59c5f574d6f7aca7e
Sohan Murthy <sohan.murthy@gmail.com>
2020-06-22 08:28:01 -0700

    adds hospitalization visuals

--- scripts/covid_visuals.R
+++ scripts/covid_visuals.R
@@ -5,7 +5,7 @@ library(reshape2)
 #state cumulative and 7 day rolling case positivity
 state_daily.df %>%
   filter(date >= today() - 60,
-         state %in% c("FL", "AZ", "CA", "IL", "OH", "CO")
+         state %in% c("FL", "AZ", "CA", "IL", "OH", "CO", "TX")
          ) %>%
   select(date, state, pct_pos_7day, pct_pos_cume) %>%
   melt(id.vars = c("date", "state")) %>%
@@ -14,13 +14,15 @@ state_daily.df %>%
                                     )) %>%
 ggplot(aes(x = date, y = value, color = variable)) +
   geom_line() +
+  geom_hline(yintercept = 0, size = 0.25, color = color.dark_gray) +
   scale_y_continuous(labels = percent, limits = c(0, NA)) +
   scale_color_manual(values = color.carto_bold, name = "") +
-  facet_wrap(~state, ncol = 2) +
+  facet_wrap(~state, ncol = 3) +
   labs(title = "Daily & Cumulative Case Positivity", subtitle = "Select states, last 60 Days") +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
-        legend.position = "bottom") +
+        legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
   guides(color = guide_legend(override.aes = list(size=1.5)))
 
 
@@ -60,7 +62,7 @@ us_daily.df %>%
 
 #walk graph state
 state_daily.df %>%
-filter(state == "CA",
+filter(state == "TX",
        date >= today() - 60) %>%
   ggplot(aes(x = death7day, y = pct_pos_7day, size = total7day)) +
   geom_point(shape = 21, fill = color.carto_bold[2], stroke = 0, alpha = 0.3) +
@@ -74,3 +76,49 @@ filter(state == "CA",
        y = "Daily % Positive Tests") +
   theme(legend.position = "right")
 
+
+# ICU patients by State
+state_daily.df %>%
+  filter(date >= today() - 60,
+         state %in% c( "AZ", "CA", "IL", "OH")
+  ) %>%
+  select(date, state, inIcu7day, inIcuCurrently) %>%
+  melt(id.vars = c("date", "state")) %>%
+  mutate(variable = str_replace_all(variable, c("inIcu7day" = "ICU (7 day avg)",
+                                                "inIcuCurrently" = "ICU")
+  )) %>%
+  ggplot(aes(x = date, y = value, color = variable)) +
+  geom_line() +
+  geom_hline(yintercept = 0, size = 0.25, color = color.dark_gray) +
+  scale_y_continuous(labels = comma, limits = c(0, NA)) +
+  scale_color_manual(values = color.carto_bold, name = "") +
+  facet_wrap(~state, ncol = 2, scales = "free_y") +
+  labs(title = "Daily Patients in ICU", subtitle = "Select states, last 60 Days") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  guides(color = guide_legend(override.aes = list(size=1.5)))
+
+#hospitalized patients by State
+state_daily.df %>%
+  filter(date >= today() - 60,
+         state %in% c( "AZ", "CA", "IL", "OH", "NY", "NJ", "TX", "NC", "SC")
+  ) %>%
+  select(date, state, hospitalized7day, hospitalizedCurrently) %>%
+  melt(id.vars = c("date", "state")) %>%
+  mutate(variable = str_replace_all(variable, c("hospitalized7day" = "Hospitalized (7 day avg)",
+                                                "hospitalizedCurrently" = "Hospitalized")
+  )) %>%
+  ggplot(aes(x = date, y = value, color = variable)) +
+  geom_line() +
+  geom_hline(yintercept = 0, size = 0.25, color = color.dark_gray) +
+  scale_y_continuous(labels = comma, limits = c(0, NA)) +
+  scale_color_manual(values = color.carto_bold, name = "") +
+  facet_wrap(~state, ncol = 3, scales = "free_y") +
+  labs(title = "Daily Hospitalized Patients", subtitle = "Select states, last 60 Days") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  guides(color = guide_legend(override.aes = list(size=1.5)))
--- scripts/prep_data.R
+++ scripts/prep_data.R
@@ -5,6 +5,8 @@ source("utils/analytics.R")
 download.file("https://covidtracking.com/api/v1/states/daily.csv", "source/state_daily.csv")
 download.file("https://covidtracking.com/api/v1/us/daily.csv", "source/us_daily.csv")
 
+#' State data
+#' adds 7 day rolling averages and pct positive
 state_daily.df <-
 read_csv("source/state_daily.csv") %>%
   mutate(date = ymd(date),
@@ -15,9 +17,14 @@ read_csv("source/state_daily.csv") %>%
   mutate(pos7day = roll_mean_na(positiveIncrease),
          total7day = roll_mean_na(totalTestResultsIncrease),
          pct_pos_7day = pos7day / total7day,
-         death7day = roll_mean_na(deathIncrease)) %>%
+         death7day = roll_mean_na(deathIncrease),
+         hospitalized7day = roll_mean_na(hospitalizedCurrently),
+         inIcu7day = roll_mean_na(inIcuCurrently)) %>%
   ungroup()
 
+
+#' US data
+#' adds 7 day rolling averages and pct positive
 us_daily.df <-
   read.csv("source/us_daily.csv") %>%
   mutate(date = ymd(date),
@@ -26,5 +33,7 @@ us_daily.df <-
          pos7day = roll_mean_na(positiveIncrease),
          total7day = roll_mean_na(totalTestResultsIncrease),
          pct_pos_7day = pos7day / total7day,
-         death7day = roll_mean_na(deathIncrease))
+         death7day = roll_mean_na(deathIncrease),
+         hospitalized7day = roll_mean_na(hospitalizedCurrently),
+         inIcu7day = roll_mean_na(inIcuCurrently))
 
