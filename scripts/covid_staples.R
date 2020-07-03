574fb2693d162b7465fc7d626cfa3cae06a0edec
Sohan Murthy <sohan.murthy@gmail.com>
2020-07-03 11:56:29 -0700

    tweaks frame rates to walkgraphs

--- scripts/covid_staples.R
+++ scripts/covid_staples.R
@@ -57,7 +57,15 @@ region_daily.df %>%
 ggsave("output/region_hospitalized_grid.png", dpi = 150, width = 7, height = 6)
 
 
-#walk graph US hospitalizations
+
+#' WALKGRAPHS
+#' Animating daily hospitalizations by case positivity
+
+#set frame length
+walkgraph_frames <-
+  as.numeric(us_daily.df %>% select(date) %>% top_n(1) %>% pull(date) - as.Date("2020-03-15"))
+
+#US hospitalizations
 us_daily.plot <-
   us_daily.df %>%
   mutate(date_abbr = paste0(month(date, label = TRUE),'. ', day(date),', ', year(date))) %>%
@@ -71,15 +79,6 @@ us_daily.plot <-
   scale_size_continuous(range = c(1, 8), name = "Daily Tests", label = label_number_si()) +
   theme(legend.position = "right")
 
-
-us_daily.plot +
-  #geom_text(aes(label = date_abbr), size = 2, check_overlap = TRUE) +
-  labs(title = "Daily Hospitalizations vs. Case Positivity",
-       subtitle = "Seven-day rolling averages, since March 15, 2020\nSource: covidtesting.com",
-       x = "Hospitalizations",
-       y = "Case Positivity")
-
-#walk graph US hospitalizations animation
 us_daily.anim <-
   us_daily.plot +
   labs(title = "US COVID-19 Hospitalizations vs. Case Positivity",
@@ -88,13 +87,19 @@ us_daily.anim <-
        y = "Case Positivity") +
   transition_manual(date, cumulative = TRUE)
 
-animate(us_daily.anim, fps = 4, end_pause = 25, rewind = FALSE,
-        width = 900, height = 850, res = 150)
+animate(us_daily.anim,
+        nframes = walkgraph_frames,
+        fps = 4,
+        end_pause = 30,
+        rewind = FALSE,
+        width = 900,
+        height = 850,
+        res = 150)
 
 anim_save("output/us_hosp_walkgraph.gif")
 
 
-#walk graph regional hospitalizations
+#Regional hospitalizations
 region_daily.plot <-
   region_daily.df %>%
   mutate(date_abbr = paste0(month(date, label = TRUE),'. ', day(date),', ', year(date))) %>%
@@ -109,13 +114,6 @@ region_daily.plot <-
   scale_size_continuous(range = c(0.5, 6), name = "Daily Tests", label = label_number_si()) +
   theme(legend.position = "right")
 
-#static image
-region_daily.plot +
-  #geom_text(aes(label = date_abbr), size = 2, check_overlap = TRUE) +
-  labs(title = "Daily Hospitalizations vs. Case Positivity",
-       subtitle = "Seven-day rolling averages, since March 15, 2020\nSource: covidtesting.com",
-       x = "Hospitalizations",
-       y = "Case Positivity")
 
 #animation
 region_daily.anim <-
@@ -126,157 +124,14 @@ region_daily.anim <-
        y = "Case Positivity") +
   transition_manual(date, cumulative = TRUE)
 
-animate(region_daily.anim, fps = 4, end_pause = 30, rewind = FALSE,
-        width = 900, height = 875, res = 150)
+animate(region_daily.anim,
+        nframes = walkgraph_frames,
+        fps = 4,
+        end_pause = 30,
+        rewind = FALSE,
+        width = 900,
+        height = 875,
+        res = 150)
 
 anim_save("output/regional_hosp_walkgraph.gif")
 
-
-#walk graph US ICU admissions
-us_daily.plot <-
-  us_daily.df %>%
-  mutate(date_abbr = paste0(month(date, label = TRUE),'. ', day(date),', ', year(date))) %>%
-  filter(date >= "2020-03-15") %>%
-  ggplot(aes(x = inIcu7day, y = pct_pos_7day, size = total7day)) +
-  geom_point(shape = 21, fill = color.carto_bold[2], stroke = 0, alpha = 0.3) +
-  geom_path(size = 0.5, color = "black") +
-  geom_hline(yintercept = 0, size = 0.25, color = color.dark_gray) +
-  scale_y_continuous(label = percent, limits = c(0,NA)) +
-  scale_x_continuous(label = comma, limits = c(0,NA)) +
-  scale_size_continuous(range = c(1, 8), name = "Daily Tests", label = label_number_si()) +
-  theme(legend.position = "right")
-
-
-us_daily.plot +
-  #geom_text(aes(label = date_abbr), size = 2, check_overlap = TRUE) +
-  labs(title = "Daily ICU Admissions vs. Case Positivity",
-       subtitle = "Seven-day rolling averages, since March 15, 2020\nSource: covidtesting.com",
-       x = "ICU Admissions",
-       y = "Case Positivity")
-
-#walk graph US hospitalizations animation
-us_daily.anim <-
-  us_daily.plot +
-  labs(title = "US COVID-19 ICU Admissions vs. Case Positivity",
-       subtitle = "Seven-day rolling averages for { current_frame }\nSource: covidtracking.com",
-       x = "ICU Admissions",
-       y = "Case Positivity") +
-  transition_manual(date, cumulative = TRUE)
-
-animate(us_daily.anim, fps = 4, end_pause = 25, rewind = FALSE,
-        width = 900, height = 850, res = 150)
-
-anim_save("output/us_icu_walkgraph.gif")
-
-
-#walk graph regional ICU admissions
-region_daily.plot <-
-  region_daily.df %>%
-  mutate(date_abbr = paste0(month(date, label = TRUE),'. ', day(date),', ', year(date))) %>%
-  filter(date >= "2020-03-15") %>%
-  ggplot(aes(x = inIcu7day, y = pct_pos_7day, size = total7day)) +
-  geom_point(shape = 21, fill = color.carto_bold[2], stroke = 0, alpha = 0.3) +
-  geom_path(size = 0.5, color = "black") +
-  geom_hline(yintercept = 0, size = 0.25, color = color.dark_gray) +
-  facet_wrap(~region, ncol = 2) +
-  scale_y_continuous(label = percent, limits = c(0,NA)) +
-  scale_x_continuous(label = label_number_si(), limits = c(0,NA)) +
-  scale_size_continuous(range = c(0.5, 6), name = "Daily Tests", label = label_number_si()) +
-  theme(legend.position = "right")
-
-#static image
-region_daily.plot +
-  #geom_text(aes(label = date_abbr), size = 2, check_overlap = TRUE) +
-  labs(title = "Daily ICU Admissions vs. Case Positivity",
-       subtitle = "Seven-day rolling averages, since March 15, 2020\nSource: covidtesting.com",
-       x = "ICU Admissions",
-       y = "Case Positivity")
-
-#animation
-region_daily.anim <-
-  region_daily.plot +
-  labs(title = "US COVID-19 ICU Admissions vs. Case Positivity",
-       subtitle = "Seven-day rolling averages for { current_frame }\nSource: covidtracking.com",
-       x = "ICU Admissions",
-       y = "Case Positivity") +
-  transition_manual(date, cumulative = TRUE)
-
-animate(region_daily.anim, fps = 4, end_pause = 30, rewind = FALSE,
-        width = 900, height = 875, res = 150)
-
-anim_save("output/regional_icu_walkgraph.gif")
-
-
-#walk graph US deaths
-us_daily.plot <-
-  us_daily.df %>%
-  mutate(date_abbr = paste0(month(date, label = TRUE),'. ', day(date),', ', year(date))) %>%
-  filter(date >= "2020-03-15") %>%
-  ggplot(aes(x = death7day, y = pct_pos_7day, size = total7day)) +
-  geom_point(shape = 21, fill = color.carto_bold[2], stroke = 0, alpha = 0.3) +
-  geom_path(size = 0.5, color = "black") +
-  geom_hline(yintercept = 0, size = 0.25, color = color.dark_gray) +
-  scale_y_continuous(label = percent, limits = c(0,NA)) +
-  scale_x_continuous(label = comma, limits = c(0,NA)) +
-  scale_size_continuous(range = c(1, 8), name = "Daily Tests", label = label_number_si()) +
-  theme(legend.position = "right")
-
-
-us_daily.plot +
-  #geom_text(aes(label = date_abbr), size = 2, check_overlap = TRUE) +
-  labs(title = "Daily Deaths vs. Case Positivity",
-       subtitle = "Seven-day rolling averages, since March 15, 2020\nSource: covidtesting.com",
-       x = "Deaths",
-       y = "Case Positivity")
-
-#walk graph US hospitalizations animation
-us_daily.anim <-
-  us_daily.plot +
-  labs(title = "US COVID-19 Deaths vs. Case Positivity",
-       subtitle = "Seven-day rolling averages for { current_frame }\nSource: covidtracking.com",
-       x = "Deaths",
-       y = "Case Positivity") +
-  transition_manual(date, cumulative = TRUE)
-
-animate(us_daily.anim, fps = 4, end_pause = 25, rewind = FALSE,
-        width = 900, height = 850, res = 150)
-
-anim_save("output/us_deaths_walkgraph.gif")
-
-
-#walk graph regional ICU admissions
-region_daily.plot <-
-  region_daily.df %>%
-  mutate(date_abbr = paste0(month(date, label = TRUE),'. ', day(date),', ', year(date))) %>%
-  filter(date >= "2020-03-15") %>%
-  ggplot(aes(x = death7day, y = pct_pos_7day, size = total7day)) +
-  geom_point(shape = 21, fill = color.carto_bold[2], stroke = 0, alpha = 0.3) +
-  geom_path(size = 0.5, color = "black") +
-  geom_hline(yintercept = 0, size = 0.25, color = color.dark_gray) +
-  facet_wrap(~region, ncol = 2) +
-  scale_y_continuous(label = percent, limits = c(0,NA)) +
-  scale_x_continuous(label = comma, limits = c(0,NA)) +
-  scale_size_continuous(range = c(0.5, 6), name = "Daily Tests", label = label_number_si()) +
-  theme(legend.position = "right")
-
-#static image
-region_daily.plot +
-  #geom_text(aes(label = date_abbr), size = 2, check_overlap = TRUE) +
-  labs(title = "Daily Deaths vs. Case Positivity",
-       subtitle = "Seven-day rolling averages, since March 15, 2020\nSource: covidtesting.com",
-       x = "Deaths",
-       y = "Case Positivity")
-
-#animation
-region_daily.anim <-
-  region_daily.plot +
-  labs(title = "US COVID-19 Deaths vs. Case Positivity",
-       subtitle = "Seven-day rolling averages for { current_frame }\nSource: covidtracking.com",
-       x = "Deaths",
-       y = "Case Positivity") +
-  transition_manual(date, cumulative = TRUE)
-
-animate(region_daily.anim, fps = 4, end_pause = 30, rewind = FALSE,
-        width = 900, height = 875, res = 150)
-
-anim_save("output/regional_deaths_walkgraph.gif")
